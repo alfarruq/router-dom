fc3893018fed1192a60a2cfeb89163170b173f7c
Scotny <marsithb2022@gmail.com>
2025-05-26 14:51:51 +0500

    added links to pages

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,21 +1,29 @@
 import { Link } from "react-router-dom";
 
-
-
-
 function Header() {
-    return ( 
-        <header>
-            <h3>Logo </h3>
+  return (
+    <header>
+      <h3>Logo </h3>
 
-
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/brand">Brand</Link></li>
-            </ul>
-
-        </header>
-     );
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/brand">Brand</Link>
+        </li>
+        <li>
+          <Link to="/advantages">Advantages</Link>
+        </li>
+        <li>
+          <Link to="/club">Club</Link>
+        </li>
+        <li>
+          <Link to="/contacts">Contacts</Link>
+        </li>
+      </ul>
+    </header>
+  );
 }
 
 export default Header;
