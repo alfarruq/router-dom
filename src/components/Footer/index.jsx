c654697a42581024ad86617c74ae47f3d8da1740
Scotny <marsithb2022@gmail.com>
2025-05-27 21:01:12 +0500

    added header and footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,12 +1,85 @@
-
-
+import { Link } from "react-router-dom";
 
 function Footer() {
-    return ( 
+  return (
+    <footer>
+      <nav className="container">
         <div>
-            <h2>Footer</h2>
+          <b>КАТАЛОГ ТОВАРОВ</b>
+          <ul>
+            <li>
+              <a href="">Кардио тренажеры</a>
+            </li>
+            <li>
+              <a href="">Composite Strength</a>
+            </li>
+            <li>
+              <a href="">True Stretch</a>
+            </li>
+            <li>
+              <a href="">Сайклинг</a>
+            </li>
+            <li>
+              <a href="">Групповые тренировки</a>
+            </li>
+            <li>
+              <a href="">Силовые тренажеры</a>
+            </li>
+            <li>
+              <a href="">Консоли</a>
+            </li>
+            <li>
+              <b>© TRUE FITNESS</b>
+            </li>
+          </ul>
         </div>
-     );
+        <div>
+          <b>ИНФОРМАЦИЯ</b>
+          <ul>
+            <li>
+              <Link to="/">
+                <span>главная</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/brand">
+                <span>о бренде</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/advantages">
+                <span>преимущества</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/club">
+                <span>открыть клуб</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/contacts">
+                <span>контакты</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <b>КОНТАКТЫ</b>
+          <ul>
+            <li>
+              <a href="">Политика конфиденциальности</a>
+            </li>
+            <li>
+              <a href="">Контакты</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <b>ПОДПИСАТЬСЯ НА НОВОСТИ И АКЦИИ</b>
+        </div>
+      </nav>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,24 +3,31 @@ import logo from "../../assets/images/logo.svg";
 
 function Header() {
   return (
-    <header>
-      <img src={logo} alt="" />
-
+    <header className="container">
+      <Link to="/">
+        <img className="logo" src={logo} alt="" />
+      </Link>
+      <hr />
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/brand">
+            <span>о бренде</span>
+          </Link>
         </li>
         <li>
-          <Link to="/brand">Brand</Link>
+          <Link to="/advantages">
+            <span>преимущества</span>
+          </Link>
         </li>
         <li>
-          <Link to="/advantages">Advantages</Link>
+          <Link to="/club">
+            <span>открыть клуб</span>
+          </Link>
         </li>
         <li>
-          <Link to="/club">Club</Link>
-        </li>
-        <li>
-          <Link to="/contacts">Contacts</Link>
+          <Link to="/contacts">
+            <span>контакты</span>
+          </Link>
         </li>
       </ul>
     </header>
